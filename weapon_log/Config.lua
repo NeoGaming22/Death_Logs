a251f3a6b3358ca5b8fa55ddeef0a6f6035761eb
NeoGaming22 <48037518+NeoGaming22@users.noreply.github.com>
2020-11-24 18:41:29 -0500

    Updated Weapons Config

---
+++ weapon_log/Config.lua
@@ -0,0 +1,3 @@
+Config = { }
+Config.combatlog = true -- change to false if u dont want combat logging to be recorded
+Config.webhook = "https://discord.com/api/webhooks/780818825587327006/S-2_b4CzNstSu98zBYwA_mmkBgtAMGRpjAwHp9rQQMpvP7-AVYcfPNiyWFE5i8JJRsVv"
